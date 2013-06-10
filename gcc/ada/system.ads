eadeecc4f3cbb8710675d4dffd4aa93ba3224276
Greg Nietsky <gregory@distrotech.co.za>
2013-06-10 13:41:45 +0200

    Cross compile and x32 support ADA

--- gcc/ada/system.ads
+++ gcc/ada/system.ads
@@ -39,11 +39,6 @@
 --  "proper" System, and since the compiler itself does not care about most
 --  System parameters, this generic version works fine.
 
-pragma Restrictions (No_Implicit_Dynamic_Code);
---  We want to avoid trampolines in the compiler, so it can be used in systems
---  which prevent execution of code on the stack, e.g. in windows environments
---  with DEP (Data Execution Protection) enabled.
-
 package System is
    pragma Pure;
    --  Note that we take advantage of the implementation permission to make
